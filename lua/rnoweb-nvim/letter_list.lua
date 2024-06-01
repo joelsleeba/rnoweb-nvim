0cf5029e61786759ee8a978a5d97ab1ff7d468a2
joelsleeba <joelsleeba1@gmail.com>
2024-06-01 18:17:11 +0530

    Added mathletter concealing
    The `get_letterface` function is merely a hack of the `get_diacritic`
    function. Tests required is the logic suits the new function.

---
+++ lua/rnoweb-nvim/letter_list.lua
@@ -0,0 +1,363 @@
+local M = { letterface = {} }
+
+M.letterface["A"] = {
+	["\\mathbb"] = "𝔸",
+	["\\mathcal"] = "𝓐",
+	["\\mathfrak"] = "𝔄",
+	["\\mathscr"] = "𝒜",
+	["\\mathsf"] = "𝖠",
+}
+
+M.letterface["B"] = {
+	["\\mathbb"] = "𝔹",
+	["\\mathcal"] = "𝓑",
+	["\\mathfrak"] = "𝔅",
+	["\\mathscr"] = "ℬ",
+	["\\mathsf"] = "𝖡",
+}
+
+M.letterface["C"] = {
+	["\\mathbb"] = "ℂ",
+	["\\mathcal"] = "𝓒",
+	["\\mathfrak"] = "ℭ",
+	["\\mathscr"] = "𝒞",
+	["\\mathsf"] = "𝖢",
+}
+
+M.letterface["D"] = {
+	["\\mathbb"] = "𝔻",
+	["\\mathcal"] = "𝓓",
+	["\\mathfrak"] = "𝔇",
+	["\\mathscr"] = "𝒟",
+	["\\mathsf"] = "𝖣",
+}
+
+M.letterface["E"] = {
+	["\\mathbb"] = "𝔼",
+	["\\mathcal"] = "𝓔",
+	["\\mathfrak"] = "𝔈",
+	["\\mathscr"] = "ℰ",
+	["\\mathsf"] = "𝖤",
+}
+
+M.letterface["F"] = {
+	["\\mathbb"] = "𝔽",
+	["\\mathcal"] = "𝓕",
+	["\\mathfrak"] = "𝔉",
+	["\\mathscr"] = "ℱ",
+	["\\mathsf"] = "𝖥",
+}
+
+M.letterface["G"] = {
+	["\\mathbb"] = "𝔾",
+	["\\mathcal"] = "𝓖",
+	["\\mathfrak"] = "𝔊",
+	["\\mathscr"] = "𝒢",
+	["\\mathsf"] = "𝖦",
+}
+
+M.letterface["H"] = {
+	["\\mathbb"] = "ℍ",
+	["\\mathcal"] = "𝓗",
+	["\\mathfrak"] = "ℌ",
+	["\\mathscr"] = "ℋ",
+	["\\mathsf"] = "𝖧",
+}
+
+M.letterface["I"] = {
+	["\\mathbb"] = "𝕀",
+	["\\mathcal"] = "𝓘",
+	["\\mathfrak"] = "ℑ",
+	["\\mathscr"] = "ℐ",
+	["\\mathsf"] = "𝖨",
+}
+
+M.letterface["J"] = {
+	["\\mathbb"] = "𝕁",
+	["\\mathcal"] = "𝓙",
+	["\\mathfrak"] = "𝔍",
+	["\\mathscr"] = "𝒥",
+	["\\mathsf"] = "𝖩",
+}
+
+M.letterface["K"] = {
+	["\\mathbb"] = "𝕂",
+	["\\mathcal"] = "𝓚",
+	["\\mathfrak"] = "𝔎",
+	["\\mathscr"] = "𝒦",
+	["\\mathsf"] = "𝖪",
+}
+
+M.letterface["L"] = {
+	["\\mathbb"] = "𝕃",
+	["\\mathcal"] = "𝓛",
+	["\\mathfrak"] = "𝔏",
+	["\\mathscr"] = "ℒ",
+	["\\mathsf"] = "𝖫",
+}
+
+M.letterface["M"] = {
+	["\\mathbb"] = "𝕄",
+	["\\mathcal"] = "𝓜",
+	["\\mathfrak"] = "𝔐",
+	["\\mathscr"] = "ℳ",
+	["\\mathsf"] = "𝖬",
+}
+
+M.letterface["N"] = {
+	["\\mathbb"] = "ℕ",
+	["\\mathcal"] = "𝓝",
+	["\\mathfrak"] = "𝔑",
+	["\\mathscr"] = "𝒩",
+	["\\mathsf"] = "𝖭",
+}
+
+M.letterface["O"] = {
+	["\\mathbb"] = "𝕆",
+	["\\mathcal"] = "𝓞",
+	["\\mathfrak"] = "𝔒",
+	["\\mathscr"] = "𝒪",
+	["\\mathsf"] = "𝖮",
+}
+
+M.letterface["P"] = {
+	["\\mathbb"] = "ℙ",
+	["\\mathcal"] = "𝓟",
+	["\\mathfrak"] = "𝔓",
+	["\\mathscr"] = "𝒫",
+	["\\mathsf"] = "𝖯",
+}
+
+M.letterface["Q"] = {
+	["\\mathbb"] = "ℚ",
+	["\\mathcal"] = "𝓠",
+	["\\mathfrak"] = "𝔔",
+	["\\mathscr"] = "𝒬",
+	["\\mathsf"] = "𝖰",
+}
+
+M.letterface["R"] = {
+	["\\mathbb"] = "ℝ",
+	["\\mathcal"] = "𝓡",
+	["\\mathfrak"] = "ℜ",
+	["\\mathscr"] = "ℛ",
+	["\\mathsf"] = "𝖱",
+}
+
+M.letterface["S"] = {
+	["\\mathbb"] = "𝕊",
+	["\\mathcal"] = "𝓢",
+	["\\mathfrak"] = "𝔖",
+	["\\mathscr"] = "𝒮",
+	["\\mathsf"] = "𝖲",
+}
+
+M.letterface["T"] = {
+	["\\mathbb"] = "𝕋",
+	["\\mathcal"] = "𝓣",
+	["\\mathfrak"] = "𝔗",
+	["\\mathscr"] = "𝒯",
+	["\\mathsf"] = "𝖳",
+}
+
+M.letterface["U"] = {
+	["\\mathbb"] = "𝕌",
+	["\\mathcal"] = "𝓤",
+	["\\mathfrak"] = "𝔘",
+	["\\mathscr"] = "𝒰",
+	["\\mathsf"] = "𝖴",
+}
+
+M.letterface["V"] = {
+	["\\mathbb"] = "𝕍",
+	["\\mathcal"] = "𝓥",
+	["\\mathfrak"] = "𝔙",
+	["\\mathscr"] = "𝒱",
+	["\\mathsf"] = "𝖵",
+}
+
+M.letterface["W"] = {
+	["\\mathbb"] = "𝕎",
+	["\\mathcal"] = "𝓦",
+	["\\mathfrak"] = "𝔚",
+	["\\mathscr"] = "𝒲",
+	["\\mathsf"] = "𝖶",
+}
+
+M.letterface["X"] = {
+	["\\mathbb"] = "𝕏",
+	["\\mathcal"] = "𝓧",
+	["\\mathfrak"] = "𝔛",
+	["\\mathscr"] = "𝒳",
+	["\\mathsf"] = "𝖷",
+}
+
+M.letterface["Y"] = {
+	["\\mathbb"] = "𝕐",
+	["\\mathcal"] = "𝓨",
+	["\\mathfrak"] = "𝔜",
+	["\\mathscr"] = "𝒴",
+	["\\mathsf"] = "𝖸",
+}
+
+M.letterface["Z"] = {
+	["\\mathbb"] = "ℤ",
+	["\\mathcal"] = "𝓩",
+	["\\mathfrak"] = "ℨ",
+	["\\mathscr"] = "𝒵",
+	["\\mathsf"] = "𝖹",
+}
+
+M.letterface["a"] = {
+	["\\mathfrak"] = "𝔞",
+	["\\mathsf"] = "𝖺",
+}
+
+M.letterface["b"] = {
+	["\\mathfrak"] = "𝔟",
+	["\\mathsf"] = "𝖻",
+}
+
+M.letterface["c"] = {
+	["\\mathfrak"] = "𝔠",
+	["\\mathsf"] = "𝖼",
+}
+
+M.letterface["d"] = {
+	["\\mathfrak"] = "𝔡",
+	["\\mathsf"] = "𝖽",
+}
+
+M.letterface["e"] = {
+	["\\mathfrak"] = "𝔢",
+	["\\mathsf"] = "𝖾",
+}
+
+M.letterface["f"] = {
+	["\\mathfrak"] = "𝔣",
+	["\\mathsf"] = "𝖿",
+}
+
+M.letterface["g"] = {
+	["\\mathfrak"] = "𝔤",
+	["\\mathsf"] = "𝗀",
+}
+
+M.letterface["h"] = {
+	["\\mathfrak"] = "𝔥",
+	["\\mathsf"] = "𝗁",
+}
+
+M.letterface["i"] = {
+	["\\mathfrak"] = "𝔦",
+	["\\mathsf"] = "𝗂",
+}
+
+M.letterface["j"] = {
+	["\\mathfrak"] = "𝔧",
+	["\\mathsf"] = "𝗃",
+}
+
+M.letterface["k"] = {
+	["\\mathfrak"] = "𝔨",
+	["\\mathsf"] = "𝗄",
+}
+
+M.letterface["l"] = {
+	["\\mathfrak"] = "𝔩",
+	["\\mathsf"] = "𝗅",
+}
+
+M.letterface["m"] = {
+	["\\mathfrak"] = "𝔪",
+	["\\mathsf"] = "𝗆",
+}
+
+M.letterface["n"] = {
+	["\\mathfrak"] = "𝔫",
+	["\\mathsf"] = "𝗇",
+}
+
+M.letterface["o"] = {
+	["\\mathfrak"] = "𝔬",
+	["\\mathsf"] = "𝗈",
+}
+
+M.letterface["p"] = {
+	["\\mathfrak"] = "𝔭",
+	["\\mathsf"] = "𝗉",
+}
+
+M.letterface["q"] = {
+	["\\mathfrak"] = "𝔮",
+	["\\mathsf"] = "𝗊",
+}
+
+M.letterface["r"] = {
+	["\\mathfrak"] = "𝔯",
+	["\\mathsf"] = "𝗋",
+}
+
+M.letterface["s"] = {
+	["\\mathfrak"] = "𝔰",
+	["\\mathsf"] = "𝗌",
+}
+
+M.letterface["t"] = {
+	["\\mathfrak"] = "𝔱",
+	["\\mathsf"] = "𝗍",
+}
+
+M.letterface["u"] = {
+	["\\mathfrak"] = "𝔲",
+	["\\mathsf"] = "𝗎",
+}
+
+M.letterface["v"] = {
+	["\\mathfrak"] = "𝔳",
+	["\\mathsf"] = "𝗏",
+}
+
+M.letterface["w"] = {
+	["\\mathfrak"] = "𝔴",
+	["\\mathsf"] = "𝗐",
+}
+
+M.letterface["x"] = {
+	["\\mathfrak"] = "𝔵",
+	["\\mathsf"] = "𝗑",
+}
+
+M.letterface["y"] = {
+	["\\mathfrak"] = "𝔶",
+	["\\mathsf"] = "𝗒",
+}
+
+M.letterface["z"] = {
+	["\\mathfrak"] = "𝔷",
+	["\\mathsf"] = "𝗓",
+}
+
+M.get_letterface = function(cmd, txt, node)
+	-- If we're in this command, we already know exactly where the thing we want to use is
+	local cc = node:child_count()
+	local brow, bcol, erow, ecol = node:range()
+	local nt
+	if cc > 1 then
+		nt = vim.treesitter.get_node_text(node:child(1):child(1), 0)
+	else
+		local line = vim.api.nvim_buf_get_lines(0, erow, erow + 1, true)
+		nt = string.sub(line[1], ecol + 1, ecol + 1)
+		ecol = ecol + 1
+	end
+
+	if M.letterface[nt] ~= nil then
+		if M.letterface[nt][cmd] ~= nil then
+			local val = { txt = { M.letterface[nt][cmd] }, full = true, bcol = bcol, ecol = ecol }
+			return val
+		end
+	end
+	return txt
+end
+
+return M
